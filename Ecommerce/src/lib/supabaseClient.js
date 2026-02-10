5f7f1d8ab1e289be48b8ce8c78366a486eb4b202
Prasan <thapaprasan058@gmail.com>
2026-02-10 15:55:50 +0545

    added a login logout continue with google

--- Ecommerce/src/lib/supabaseClient.js
+++ Ecommerce/src/lib/supabaseClient.js
@@ -1,11 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
-if (!supabaseUrl || !supabaseAnonKey) {
-    // eslint-disable-next-line no-console
-    console.warn('Supabase URL or Key is missing. Check your .env setup.');
-}
-
-export const supabase = createClient(supabaseUrl || '', supabaseAnonKey || '');
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+)
